4e2bc8c3be95f0b222d0c18e0fd7788ff87645a7
BOURGET Hugo <h.bourget@eleve.supalta.com>
2021-10-06 10:22:04 +0200

    Initial commit

---
+++ fonctions.h
@@ -0,0 +1,32 @@
+//
+// Created by h.bourget on 06/10/2021.
+//
+
+#ifndef BENCHME_FONCTIONS_H
+#define BENCHME_FONCTIONS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+public char *triBulle(char *tab)
+{
+
+}
+
+public char *triSelection(char *tab)
+{
+
+}
+
+public char *triInsertion(char *tab)
+{
+
+}
+
+public char *triTas(char *tab)
+{
+
+}
+
+#endif //BENCHME_FONCTIONS_H
