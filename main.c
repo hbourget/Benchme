13e9cfa31316bd9cbfd0a9da980bf8ae7bba2bed
hbourget <57684366+hbourget@users.noreply.github.com>
2021-10-06 21:41:40 +0200

    chrono

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <time.h>
 #include "fonctions.h"
 
 int main()
 {
-    int taille = 100;
+    int taille = 25000;
     float *tab = (float*) malloc(sizeof(float)*taille);
+    srand(1);
     for (int i=0;i<taille;i++)
     {
         tab[i] = ((float)rand() / (float)(RAND_MAX)) * taille;
     }
 
+    clock_t debut = clock();
     triBulle(tab, taille);
+    clock_t fin = clock();
 
     for(int i = 0; i < taille; i++)
     {
         printf("\n%f ", tab[i]);
     }
+
+    float time_spent = (float)(debut - fin) / CLOCKS_PER_SEC;
+    printf("\nTemps exec: %f", time_spent);
 }
