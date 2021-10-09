54d006d473966e7434274036f169c3cce93f5a27
hbourget <57684366+hbourget@users.noreply.github.com>
2021-10-09 12:41:46 +0200

    cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/Benchme.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Admin/CLionProjects/Benchme/bench.c" "C:/Users/Admin/CLionProjects/Benchme/cmake-build-debug/CMakeFiles/Benchme.dir/bench.c.obj"
+  "C:/Users/Admin/CLionProjects/Benchme/main.c" "C:/Users/Admin/CLionProjects/Benchme/cmake-build-debug/CMakeFiles/Benchme.dir/main.c.obj"
+  "C:/Users/Admin/CLionProjects/Benchme/misc.c" "C:/Users/Admin/CLionProjects/Benchme/cmake-build-debug/CMakeFiles/Benchme.dir/misc.c.obj"
+  "C:/Users/Admin/CLionProjects/Benchme/tri.c" "C:/Users/Admin/CLionProjects/Benchme/cmake-build-debug/CMakeFiles/Benchme.dir/tri.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
